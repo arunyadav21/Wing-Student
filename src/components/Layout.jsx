e2378dfa7e1f784da850a15b65777e3838fcef51
Arun Yadav <arun305565@gmail.com>
2025-11-20 00:52:18 +0530

    fixed

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,12 +1,12 @@
 import { Outlet } from "react-router-dom";
 import Footer from "./Footer";
-import Navbar from "./Navbar";
+import NavBar from './NavBar';
 
 export default function Layout() {
   return (
     <div className="flex flex-col min-h-screen">
       {/* ✅ Navbar appears on every page */}
-      {/* <Navbar /> */}
+      <NavBar />
 
 
 
