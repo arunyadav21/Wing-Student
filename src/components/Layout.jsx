e1c9ca123c359b4d8c5f638c3fee5d4ef60f81b4
Arun Yadav <arun305565@gmail.com>
2025-11-20 00:48:50 +0530

    navbar commented

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -6,7 +6,7 @@ export default function Layout() {
   return (
     <div className="flex flex-col min-h-screen">
       {/* ✅ Navbar appears on every page */}
-      <Navbar />
+      {/* <Navbar /> */}
 
 
 
